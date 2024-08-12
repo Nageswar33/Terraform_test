6f81bca5c1540ace427ff3530ed1f4367e890a56
Nageswara Rao D <nageswararao.dolla@gmail.com>
2024-08-12 10:47:45 +0530

    Teraform1

--- Test_1/1.tf
+++ Test_1/1.tf
@@ -1,9 +1,9 @@
-resource "aws_instance" "web" {
-  ami           = "ami-041e2ea9402c46c32"
+resource "aws_instance" "Nag" {
+  ami           = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-034a98c1bcbfe9aa0"]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Test_1"
   }
 }
