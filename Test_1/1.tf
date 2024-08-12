898e301e122cf48202d8268b01c8957b6e791cba
Nageswara Rao D <nageswararao.dolla@gmail.com>
2024-08-12 11:41:25 +0530

    Teraform1

--- Test_1/1.tf
+++ Test_1/1.tf
@@ -9,12 +9,6 @@ resource "aws_instance" "Nag2" {
   }
 }
 
-data "aws_ami" "ami" {
-  most_recent = true
- // name_regex  = "Cent*"
-  owners = [ "891377247566" ]
-}
-
 output  "ami" {
-  value = data.aws_ami.ami
+  value = aws_instance.Nag2
 }
