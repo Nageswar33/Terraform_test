80f4b3e2f0455712429edd65a556780ef8f4cc7c
Nageswara Rao D <nageswararao.dolla@gmail.com>
2024-08-15 15:43:25 +0530

    Teraform1

---
+++ Multiple_Ec2s/main.tf
@@ -0,0 +1,31 @@
+variable "ami" {
+  default = "ami-0b4f379183e5706b9"
+}
+variable "security_groups" {
+  default = [ "sg-034a98c1bcbfe9aa0" ]
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "components"{
+  default = {
+    frontend = { }
+    mongodb = {}
+    mysql = {}
+    rebbitmq = {}
+  }
+}
+
+resource "aws_instance" "instance" {
+
+  for_each = var.components
+   ami = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_groups
+
+  tags = {
+    Name = lookup(each.key , key,null)
+  }
+}
