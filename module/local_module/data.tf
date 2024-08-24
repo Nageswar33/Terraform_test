f74fafebbcd86e03af90cd9bbef322668e99e396
Nageswara Rao D <nageswararao.dolla@gmail.com>
2024-08-24 15:53:49 +0530

    Teraform1

--- module/local_module/data.tf
+++ module/local_module/data.tf
@@ -1,12 +1,15 @@
 
 data "aws_ami" "ami"{
+  ami = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group
 
 }
 
 resource "aws_instance" "instance"{
-  ami = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_group
+  ami = data.awi
+  instance_type = data.instance_type
+  vpc_security_group_ids = data.security_group
 
   tags = {
      Name = "test_1"
