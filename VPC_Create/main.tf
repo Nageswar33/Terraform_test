56b1e352908453898b62967aa68480cfdab65bb2
Nageswara Rao D <nageswararao.dolla@gmail.com>
2024-09-22 10:04:10 +0530

    Teraform1

--- VPC_Create/main.tf
+++ VPC_Create/main.tf
@@ -1,8 +1,11 @@
-module "components" {
+module "vpc" {
   source = "git::https://github.com/Nageswar33/tf_module_vpc.git"
 
   for_each = var.vpc
   cidr = each.value["cidr"]
   subnets = each.value["subnets"]
+}
 
+output "vpc"{
+  value = module.vpc
 }
